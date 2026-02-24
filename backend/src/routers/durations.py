0371f385ac5b3f4f9ab56a225ef05d2cb2fd59db
l0gnes <loonargt@gmail.com>
2026-02-24 19:42:41 -0400

    + fetching durations by day
    ... eating slommy by night, hell yeah

--- backend/src/routers/durations.py
+++ backend/src/routers/durations.py
@@ -1,3 +1,5 @@
+from typing import Annotated
+
 from fastapi.routing import APIRouter
 from fastapi import HTTPException, Path
 from datetime import date
@@ -5,7 +7,7 @@ from datetime import date
 from ..db import get_session as get_db_session
 from ..db.helpers import evil_duration_fetching_function
 from ..dependencies.auth import TokenDependencyType
-from ..wakatime import WakatimeISOWeekTimeframe
+from ..wakatime import WakatimeISOWeekTimeframe, WakatimeSingleDayTimeframe
 from ..models.durations import (
     DurationResponseModel,
     BulkDurationResponseModel,
@@ -72,6 +74,88 @@ async def get_durations_for_week(
         await session.commit()
 
         return BulkDurationResponseModel(durations=duration_responses)
+
+@router.get("/durations/day")
+async def get_duration_for_today(
+    tokens : TokenDependencyType
+) -> DurationResponseModel:
+    """
+    Returns the user's durations for today.
+    """
+    today = date.today()
+
+    return await get_durations_for_day(
+        tokens = tokens,
+        year = today.year,
+        month = today.month,
+        day = today.day
+    )
+
+@router.get("/durations/day/{year}/{month}/{day}")
+async def get_durations_for_day(
+    tokens : TokenDependencyType,
+    year : int,
+    month : Annotated[int, Path(ge=1, le=12)],
+    day : Annotated[int, Path(ge=1, le=31)]
+) -> DurationResponseModel:
+    """
+    Returns a single duration response for the provided day
+    """
+
+    # Construct a date object from args
+    date_object = date(
+        year = year,
+        month = month,
+        day = day
+    )
+
+    async with get_db_session() as session:
+
+        try:
+            durations_list = await evil_duration_fetching_function(
+                session = session,
+                tokens = tokens,
+                timeframe = WakatimeSingleDayTimeframe(day=date_object)
+            )
+        
+        except ValueError:
+            # A ValueError usually gets raised by the above call if the response 
+            # from wakatime is non-OK on a recache.
+            raise HTTPException(
+                status_code=500,
+                detail="Failed to fetch new durations from wakatime."
+            )
+        
+        if len(durations_list) != 1:
+            raise HTTPException(
+                status_code=500,
+                detail="Retrieved non-singular or null result from evil duration fetching function"
+            )
+        
+        # Since we know the length has to be one, then we know
+        # we can get the single duration
+        duration = durations_list[0]
+
+        # We construct the model here because we need to do it BEFORE
+        # committing so we dont lose access to the `WakatimeDuration` 
+        # sqlalchemy model.
+        resp_model = DurationResponseModel(
+            date = duration.date,
+            languages = [
+                LanguageBreakdownModel(
+                    name = lang.language, 
+                    total_seconds = lang.total_seconds
+                )
+                for lang in duration.languages
+            ],
+            total_seconds = duration.total_seconds,
+            last_cached_at = duration.last_cached_at
+        )
+
+        # The nefarious aforementioned commit.
+        await session.commit()
+
+    return resp_model
 
 
 __all__ = ["router"]
