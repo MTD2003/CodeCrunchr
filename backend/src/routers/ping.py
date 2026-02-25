c27a82ab8b725880c3521ed38956c1750feb5dcb
l0gnes <loonargt@gmail.com>
2026-02-25 15:17:46 -0400

    > trim ping endpoint down

--- backend/src/routers/ping.py
+++ backend/src/routers/ping.py
@@ -1,25 +1,19 @@
 from fastapi.responses import JSONResponse
 from fastapi.routing import APIRouter
-from ..db import get_session
-from sqlalchemy import literal, select
 
-
-router = APIRouter()
-
+router = APIRouter(
+    tags=["debug"]
+)
 
 @router.get("/ping")
 async def ping_ping() -> JSONResponse:
     """
-    Returns a ping response, and also checks to see if
-    the database is working.
+    Returns a simple ping response.
+
+    Mainly meant to be used for ready/liveliness checks
+    in the hosted environment.
     """
 
-    # uber simple "SELECT 1" query to test db connectivity
-    async with get_session() as session:
-        res = await session.execute(select(literal(1)))
-
-    # return a juicy json response with both true values (fingers-crossed)
-    return JSONResponse({"api_ok": True, "db_ok": res.scalar_one() == 1})
-
+    return JSONResponse({"api_ok": True})
 
 __all__ = ["router"]
