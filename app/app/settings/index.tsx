1ec0902387e33bfd338f7b5d3a15a148d74473d1
Michael <155474152+MTD2003@users.noreply.github.com>
2026-03-13 18:03:50 -0300

    Setup directories
    TODO: Make layouts, functionality (so everything).

---
+++ app/app/settings/index.tsx
@@ -0,0 +1,19 @@
+import { Text, View } from "react-native";
+
+export default function SettingsScreen() {
+  return (
+    <View
+      style={styles}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create = ({
+    {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    }
+})
